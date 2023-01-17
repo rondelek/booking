d4a84531cc8e9b31eaf95ddb988e692277e2df66
Ewelina Nowak <ewelinanowak@Ewelinas-MacBook-Pro.local>
2023-01-17 20:05:18 +0100

    de;eted card

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import HowItWorks from "./HowItWorks";
 import PopularCategories from "./PopularCategories";
 import PopularPlaces from "./PopularPlaces";
 import Searchbox from "./Searchbox";
-import WhatCustomersSay from "./WhatCustomersSay";
 
 export default function Home() {
   return (
